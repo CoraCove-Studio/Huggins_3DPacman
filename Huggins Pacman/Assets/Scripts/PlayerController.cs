ae6f1e27c9e1f3ca6c6aceb13323d021f06df242
Rachel Huggins <rhuggins7441@gmail.com>
2024-01-21 14:31:51 -0500

    Saving Player Movement

---
+++ Huggins Pacman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: 3D Pacman
+//Name: Rachel Huggins
+//Section: SGD285.4171
+//Instructor: Aurore Locklear
+//Date: 01/21/2024
+/////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //components needed to be reference for later assignment
+    private Rigidbody rb;
+
+    //accessible variables in the editor
+    [SerializeField]
+    private float speed = 3.0f;
+
+    private void Start()
+    {
+        //componenets from game object needed to be assigned
+        rb = GetComponent<Rigidbody>();
+    }
+    //called before any physics calculations
+    private void FixedUpdate()
+    {
+        //grabbing input from keyboard for player 
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //creating a Vector3 to change the transform of player multiplied by the speedy
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        //execution of using Vector3 and speed
+        rb.AddForce(movement * speed);
+
+    }
+}
